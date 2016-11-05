f686acce41ce2b8c8aeabbb5b6c01d76634d0eed
Mary Gomez <mary.gomez.america@gmail.com>
2016-11-05 22:07:10 +1100

    use flux

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,34 +1,47 @@
 import TweetBox from './components/TweetBox';
 import TweetList from './components/TweetList';
+import TweetActions from './actions/TweetActions';
+import TweetStore from './stores/TweetStore';
 
+TweetActions.getAllTweets();
+
+let getAppState = () => {
+  return { tweetsList: TweetStore.getAll() };
+}
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { tweetsList: [] }
+    this.state = getAppState();
+    this._onChange = this._onChange.bind(this);
   }
   formattedTweets(tweetsList) {
-    let formattedList = tweetsList.map(tweet => {
-      tweet.formattedDate = moment(tweet.created_at).fromNow();
-      return tweet;
-    });
-    return {
-      tweetsList: formattedList
-    };
+    // let formattedList = tweetsList.map(tweet => {
+    //   tweet.formattedDate = moment(tweet.created_at).fromNow();
+    //   return tweet;
+    // });
+    // return {
+    //   tweetsList: formattedList
+    // };
   }
   addTweet(tweetToAdd) {
-     $.post("/tweets", { body: tweetToAdd })
-     .success( savedTweet => {
-       let newTweetsList = this.state.tweetsList;
-       newTweetsList.unshift(savedTweet);
-       this.setState(this.formattedTweets(newTweetsList));
-     })
-     .error(error => console.log(error));
+    //  $.post("/tweets", { body: tweetToAdd })
+    //  .success( savedTweet => {
+    //    let newTweetsList = this.state.tweetsList;
+    //    newTweetsList.unshift(savedTweet);
+    //    this.setState(this.formattedTweets(newTweetsList));
+    //  })
+    //  .error(error => console.log(error));
   }
   componentDidMount() {
-    $.ajax("/tweets")
-    .success(data => this.setState(this.formattedTweets(data)))
-    .error(error => console.log(error));
+    TweetStore.addChangeListener(this._onChange)
+  }
+  componentWillUnmount() {
+    TweetStore.removeChangeListener(this._onChange)
+  }
+  _onChange() {
+    console.log(5, "Main._onChange");
+    this.setState(getAppState());
   }
   render() {
     return (
